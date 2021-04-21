81ba8b9927bc109ff3446811783a754c3ecd4173
nombregenerico <62416784+nombregenerico@users.noreply.github.com>
2021-04-21 01:52:36 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ejercicio2
+{
+    public sealed class VariableCapturadExpresionLambda
+    {
+        public static Func<int> IncrementarContadorLocal()
+        {
+            int contador = 0;
+
+            // Retorna una expresión lambda que será asignada 
+            // a un delegado de naturaleza `Func<TResult>`:
+            return () => contador++;
+        }
+
+        public static void Main()
+        {
+            // Creamos un delegado de tipo `Func<TResult>`:
+            Func<int> delegadoIncremento;
+
+            // Invocamos a `IncrementarContadorLocal` una 
+            // sola vez para obtener la referencia a una expresión lambda:
+            delegadoIncremento = IncrementarContadorLocal();
+
+            // Mostremos en pantalla la invocación indirecta de la 
+            // expresión lambda retornada por `IncrementarContadorLocal` 
+            // y encapsulada en en la instancia `delegadoIncremento`:
+            Console.WriteLine(delegadoIncremento());
+            Console.WriteLine(delegadoIncremento());
+            Console.WriteLine(delegadoIncremento());
+        }
+    }
+}
